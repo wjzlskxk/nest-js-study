5a150d9fc0b24927fea08ce67515a38f1a673a3b
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-09-24 20:50:52 +0900

    init ::

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-
-@Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [],
-})
-export class AppModule {}
